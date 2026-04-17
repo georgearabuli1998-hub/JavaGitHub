8283ab07ee332b21b3c4b72a746f3aa76a9e65fd
georgearabuli1998 <georgearabuli1998@gmail.com>
2026-04-18 00:52:54 +0400

    axla vcdilob pirvelad

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main(String[] args) {
+        int a =10;
+        if(a<10) {
+            System.out.println(a);
+        }else{
+            System.out.println("tolia ");
+        }
+    }
+}

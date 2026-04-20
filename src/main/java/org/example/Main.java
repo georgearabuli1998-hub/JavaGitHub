b120d5678b5258d7fe5b27cc021263aa805a10e2
georgearabuli1998 <georgearabuli1998@gmail.com>
2026-04-20 19:29:51 +0400

    hi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,64 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
-        int a =100;
-        if(a<10) {
-            System.out.println("naklebia");
+        String clientName = "გიორგი";
+        boolean vipUser = true;
+        boolean textMassage;
+        int choosedfloor = 11;
+        int blockedFloor = 10;
+        int currentFloor =0 ;
+        int currentWeight = 100;
+        boolean canMove = false;
+        boolean doorPosition = true;
+
+        if (choosedfloor == currentFloor) {
+            doorPosition = false;
+            canMove = false;
+            choosedfloor = currentFloor;
+            System.out.println("მოგესალმებით, " + clientName + ". ჩვენ უკვე " + currentFloor + " სართულზე ვიმყოფებით");
+        }else if(choosedfloor <= -5  && vipUser == false){
+            doorPosition = false;
+            canMove = false;
+            choosedfloor = currentFloor;
+            System.out.println("მოგესალმებით, " + clientName + ". გთხოვთ აირჩიოთ ვალიდური სართული");
         }
-        if(a<=10){
-            System.out.println("tolia");
+        else if (choosedfloor >=20  && vipUser == false) {
+            doorPosition = false;
+            canMove = false;
+            choosedfloor = currentFloor;
+            System.out.println("მოგესალმებით, " + clientName + ". გთხოვთ აირჩიოთ ვალიდური სართული");
+        }else if(choosedfloor <-7  && vipUser == true){
+            doorPosition = false;
+            canMove = false;
+            choosedfloor = currentFloor;
+            System.out.println("მოგესალმებით, " + clientName + ". გთხოვთ აირჩიოთ ვალიდური სართული");
+
+        } else if(choosedfloor >50  && vipUser == true){
+            doorPosition = false;
+            canMove = false;
+            System.out.println("მოგესალმებით, " + clientName + ". გთხოვთ აირჩიოთ ვალიდური სართული");
+
+        }else if (choosedfloor == blockedFloor) {
+            doorPosition = false;
+            canMove = false;
+            choosedfloor = currentFloor;
+            System.out.println("მოგესალმებით, " + clientName + ". მოცემულ სართულზე მიმდინარეობს ტექნიკური სამუშაოები");
+        } else if (currentWeight > 400) {
+            doorPosition = false;
+            canMove = false;
+            choosedfloor = currentFloor;
+            System.out.println("მოგესალმებით, " + clientName + ". ფიქსირდება წონის გადაჭარბება");
+        }else {
+            canMove = true;
         }
-        else{
-            System.out.println("metia ");
+        if(canMove == true){
+            currentFloor = choosedfloor;
+            doorPosition=true;
+            System.out.println("მოგესალმებით, " + clientName + ". დავიძარით მოთხოვნილი სართულისკენ");
         }
+        System.out.println(doorPosition);
+        System.out.println(choosedfloor);
+        System.out.println(canMove);
     }
+
 }

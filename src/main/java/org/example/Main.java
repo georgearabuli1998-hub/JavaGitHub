33c740eb3743212db7a7b280e535efb048134e1a
georgearabuli1998 <georgearabuli1998@gmail.com>
2026-04-27 19:58:06 +0400

    pirveli saShinao davaleba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
+    /*    // ლიფტის პროექტი, ლიფტის მხრიდან.
         String clientName = "გიორგი";
         boolean vipUser = true;
         boolean textMassage;
@@ -62,6 +63,218 @@ public class Main {
         System.out.println(doorPosition);
         System.out.println(choosedfloor);
         System.out.println(canMove);
-    }
 
-}
+*/
+
+        // ავტომატიზაციის საშინაო დავალებები
+
+        // 1 .while ციკლის გამოყენებით გამოიტანეთ ეკრანზე 3 -ზე გამრავლების
+        //ტაბულა (1 - 10 ჩათვლით). "3*7=21" ასეთი სახით
+
+        /* int i = 1;
+
+        while(i<=10){
+            System.out.println( i + " * 3 = " + i * 3);
+            i++;
+        };
+
+
+        //ეს მიწერია დაბლა პასუხებში ადგილის გამოსატოვებლად დავალებებს შუა
+        System.out.println("") ;
+
+
+        //შექმენით ცვლადი და გადაეცით თქვენთვის სასურველი ნებისმიერი
+        //წინადადება და შეამოწმეთ, თუ ამ წინადადების სიგრძე იქნება ლუწი რიცხვი,
+        //დაბეჭდეთ :"ლუწი", თუ კენტი მაშინ: "კენტი"
+
+        String text = "რეგრესიული ტესტირება დავასრულე, თუმცა ლაივი გადაიდო ზოგიერთი პროექტის გამო ";
+        int textLength = text.length();
+        if(textLength % 2 == 0){
+    System.out.println(" ამ წინადადების სიგრძე არის ლუწი რიცხვი");
+        }else{
+    System.out.println("ამ წინადადების სიგრძე არის კენტი რიცხვი");
+        };
+
+
+        //ეს მიწერია დაბლა პასუხებში ადგილის გამოსატოვებლად დავალებებს შუა
+        System.out.println("") ;
+
+
+        //3  for -ით დაბეჭდეთ მასივში არსებულ ყოველი ელემენტი, გარდა მეხუთე
+        //პოზიციაზე მდგომისა
+        int[] numberList = {1,4,5,6,10,5,67,8,2,43,565,3,5};
+        int s;
+        for(s = 0; s <numberList.length; s++){
+            if(s == 4) {
+                continue;
+            }
+                System.out.println(numberList[s]) ;
+        };
+
+        //ეს მიწერია დაბლა პასუხებში ადგილის გამოსატოვებლად დავალებებს შუა
+        System.out.println("") ;
+
+
+
+        //4  შემოიტანე ნებისმიერი სტრინგი და for -ით დათვალე ამ სტრინგში რამდენი
+        //ხმოვანი არის
+        String secondText = "გოგო";
+        int xmovnebisRaodenba = 0;
+
+     for(char letter : secondText.toCharArray()){
+        if (letter == 'ა'){
+            xmovnebisRaodenba ++;
+        };
+        if (letter =='ე'){
+            xmovnebisRaodenba ++;
+        };
+        if (letter == 'ი'){
+            xmovnebisRaodenba ++;
+        };
+        if (letter == 'ო') {
+            xmovnebisRaodenba++;
+        };
+        if (letter == 'u') {
+            xmovnebisRaodenba++;
+        };
+
+        };
+        System.out.println("ტექსტში არის " + xmovnebisRaodenba + " ხმოვანი");
+
+
+        //4 მეორენაირი ამოხსნა
+        char[] xmovnebi = {'ა','ე','ი','ო','უ'};
+        int t;
+        int x;
+        int raodenoba = 0;
+        for(t=0; t<secondText.length();t++){
+            char letters = secondText.charAt(t);
+            for (x=0;x<xmovnebi.length;x++){
+                if(letters == xmovnebi[x]){
+                    raodenoba++;
+                };
+            };
+        };
+System.out.println("მეორე ვარიანტის მიხედვით, ნესთედ for-ის გამოყენებითაც ხმოვნების რაოდენობაა  " + raodenoba);
+
+
+
+        int[] cifrebi = {101,200,30,40,55,67,71,90};
+        int max = 101;
+        int[] axmas={};
+        int i;
+        for(i=0;i<cifrebi.length; i++){
+
+              cifrebi[i] = cifrebi[i] * 2;
+            System.out.println(cifrebi[i]);
+        }
+
+*/
+
+                                   // პირველი  საშინაო დავალება   27.04.2026
+
+        // დავალება   1    while ციკლის გამოყენებით გამოიტანეთ ეკრანზე 3 -ზე გამრავლების
+        //ტაბულა (1 - 10 ჩათვლით). "3*7=21" ასეთი სახით
+
+        int i =0;
+        int[] numberList = {1,2,3,4,5,6,7,8,9,10};
+        int namravli = 0;
+       while (i <numberList.length){
+           namravli = numberList[i] * 3;
+           i ++;
+           System.out.println(i +" *  3 = " + namravli);
+       };
+
+        //დავალებები გამოვყავი ერთმანეთისგან დაბლა
+        System.out.println();//დავალებები გამოვყავი დაბლა
+
+        // დავალება 2     შექმენით ცვლადი და გადაეცით თქვენთვის სასურველი ნებისმიერი
+        //წინადადება და შეამოწმეთ, თუ ამ წინადადების სიგრძე იქნება ლუწი რიცხვი,
+        //დაბეჭდეთ :"ლუწი", თუ კენტი მაშინ: "კენტი"
+
+        String textName = "1234" ;
+        if (textName.length() % 2 == 0){
+            System.out.println( "ლუწია ამ წინადადების სიგრძე");
+        }else {
+            System.out.println("კენტია ამ წინადადების სიგრძე");
+        };
+
+
+        //დავალებები გამოვყავი ერთმანეთისგან დაბლა
+        System.out.println();//დავალებები გამოვყავი დაბლა
+
+        // დავალება  3   for -ით დაბეჭდეთ მასივში არსებულ ყოველი ელემენტი, გარდა მეხუთე
+        //პოზიციაზე მდგომისა
+
+        int[] listOfNumbers = {1,2,3,4,5,6,7,8,9,10,11};
+        int j =0;
+        int textForShowing = 0;
+        for (j = 0; j< listOfNumbers.length;j++){
+            if (j == 4){
+                continue;
+            }else {
+                textForShowing = listOfNumbers[j];
+                System.out.println(textForShowing);
+            };
+        };
+
+
+        //დავალებები გამოვყავი ერთმანეთისგან დაბლა
+        System.out.println();//დავალებები გამოვყავი დაბლა
+
+
+        // დავალება  4    შემოიტანე ნებისმიერი სტრინგი და for -ით დათვალე ამ სტრინგში რამდენი
+        //ხმოვანი არის
+        String textFor4 = "ააეეიიოოუუბბბბ   ი";
+        char[] xmovnebi = {'ა','ე','ი','ო','უ'};
+        int aeiou = 0;
+
+        for (int x = 0; x < textFor4.length(); x++) {
+            char chars = textFor4.charAt(x);
+            for (int y = 0;y < xmovnebi.length;y++){
+                if (xmovnebi[y] == chars){
+                    aeiou = aeiou+1;
+                }else {
+                    continue;
+                };
+
+            };
+        };
+        System.out.println(aeiou);
+
+
+        //დავალებები გამოვყავი ერთმანეთისგან დაბლა
+        System.out.println();//დავალებები გამოვყავი დაბლა
+
+        // დავალება  5    შემოიტანე ნებისმიერი სტრინგი და while -ით დათვალე ამ სტრინგში
+        //რამდენი თანხმოვანი არის
+
+        String textFor5 = "ტატუ ,.";
+        int a = 0;
+        int tanxmovnebisRaodenoba = 0;
+        while (a < textFor5.length()) {
+            char charss = textFor5.charAt(a);
+            int b = 0;
+            boolean xmovania = false;
+            while (b<xmovnebi.length){
+
+                if (xmovnebi[b]==charss || charss == ' '){
+                    xmovania = true;
+                    break;
+
+                }else if(charss == ',' || charss == '.'){
+                    xmovania = true;
+                    break;
+                };
+                b++;
+
+            };if (!xmovania){
+                tanxmovnebisRaodenoba++;
+            };
+            a++;
+        };
+        System.out.println(tanxmovnebisRaodenoba);
+    };
+
+
+};
